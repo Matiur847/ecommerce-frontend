4e5329510345dc43fbb03c4c69553ecad188e046
Matiur847 <mclaren847329@gmail.com>
2024-03-12 01:21:54 +0600

    create updatePassword reducer

--- src/store/profileSlice/profileSlice.js
+++ src/store/profileSlice/profileSlice.js
@@ -1,10 +1,26 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const updateProfile = createAsyncThunk("/login", async (myForm) => {
+export const updateProfile = createAsyncThunk(
+  "/update-profile",
+  async (myForm) => {
+    try {
+      const config = { Headers: { "content-type": "multipart/form-data" } };
+      const response = await axios.put(
+        "/api/v1/update/profile",
+        myForm,
+        config
+      );
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+);
+
+export const updatePassword = createAsyncThunk("/update-password", async () => {
   try {
-    const config = { Headers: { "content-type": "multipart/form-data" } };
-    const response = await axios.put("/api/v1/update/profile", myForm, config);
+    const response = await axios.get("/api/v1//forgot/password");
     return response.data;
   } catch (error) {
     throw error;
@@ -35,7 +51,19 @@ const profileSlice = createSlice({
       .addCase(updateProfile.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.error.message;
-      });
+      })
+
+      .addCase(updatePassword.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(updatePassword.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.profile = action.payload;
+      })
+      .addCase(updatePassword.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      })
   },
 });
 
