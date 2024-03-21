e351c3a619e5e962111c8d7baf1c738eeaf8f032
Matiur847 <mclaren847329@gmail.com>
2024-03-22 01:42:36 +0600

    admin delete order slice

--- src/store/adminOrderListSlice/adminOrderListSlice.js
+++ src/store/adminOrderListSlice/adminOrderListSlice.js
@@ -28,7 +28,19 @@ export const adminUpdateOrder = createAsyncThunk(
   }
 );
 
-
+export const adminDeletOrder = createAsyncThunk(
+  "/admin/delete-order",
+  async (allData) => {
+    try {
+      const { id } = allData;
+      //   const config = { Headers: { "Content-Type": "application/json" } };
+      const { data } = await axios.delete(`/api/v1//admin/order/${id}`);
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+);
 
 const initialState = {
   adminOrders: [],
